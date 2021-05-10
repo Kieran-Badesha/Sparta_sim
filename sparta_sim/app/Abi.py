ba553769e042b3cd4be42421a03aa0e031d50177
AB1RAME <akumarathasan@spartaglobal.com>
2021-05-10 12:12:24 +0100

    Abi

--- sparta_sim/app/Abi.py
+++ sparta_sim/app/Abi.py
@@ -1,6 +1,5 @@
 import random
 
-
 months = int(input('enter the number of months:'))
 centres_open = months // 2
 
@@ -13,11 +12,12 @@ def generate_trainees():
     for m in range(0, months):
         trainees_per_month = random.randint(20, 30)
         trainees.append(trainees_per_month)
+    print(trainees)
     return trainees
 
 
-
 t = sum(generate_trainees())
+
 print(f'The total number of trainees: {t}')
 
 
@@ -30,29 +30,50 @@ def generate_training_centres():
 
 print(generate_training_centres())
 print(f'The number of training centres open: {centres_open}')
-item = []
+
+fc = []
 
 
+# Generate the number of full centres
 def generate_full_centres():
-    sums=0
-    for value in trainees:
+    sum_of_trainees = 0
+    # this for loop is to generate the batches of 100 trainees
+    for i in trainees:
+        sum_of_trainees += i
 
-        if sums < 100:
-            sums = sums + value
+        if sum_of_trainees <= 100:
+            pass
 
-        elif sums >= 100:
-            item.append(100)
-            sums = sums - 100
+        # this appends 100 to the fc list everytime the sum_of_trainees exceeds a 100
+        else:
+            fc.append(100)
+            sum_of_trainees -= 100
 
-    return item
+    # compares the number of batches to the centres open
+    if len(fc) <= centres_open:
+        return len(fc)
+
+    else:
+        return centres_open
 
 
-fc=generate_full_centres()
+gfc = generate_full_centres()
+print(f'The number of centres full are :{gfc}')
 
-print(f'The number of full centres are :{fc}')
+# to generate the number of trainees in the waiting list
 
 waiting_list = []
 
 
 def waiting_trainees():
     return sum(waiting_list)
+
+
+# surplus trainees added to the waiting list
+if t > centres_open * 100:
+    surplus_trainees = t - (centres_open * 100)
+    waiting_list.append(surplus_trainees)
+# this is just a placeholder for the code
+else:
+    pass
+print(waiting_list)
