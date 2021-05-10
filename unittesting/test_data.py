93f8976e0ee99fb1f4a36844560f8ac04bd87217
EBlundellSparta <EBlundellSparta@spartaglobal.com>
2021-05-10 13:59:49 +0100

    ed's branch

--- unittesting/test_data.py
+++ unittesting/test_data.py
@@ -1,13 +1,22 @@
 import unittest
 from sparta_sim.app.config_input import *
+from sparta_sim.app.Abi import waiting_trainees, waiting_list
+
 
 class test_data(unittest.TestCase):
 
+    # Tests whether months and years are integer values and total time is correct value
     def test_correct_timeframe(self):
         self.assertIsInstance(months, int)
         self.assertIsInstance(years, int)
-        self.assertEqual(total_time, (years*12)+months)
+        self.assertEqual(total_time, (years * 12) + months)
 
+    def test_waiting_list(self):
+        self.assertIsInstance(waiting_list, list)
+        try:
+            self.assertGreaterEqual(min(waiting_list), 0)
+        except:
+            print("List is empty")
 
     # def test_gen_trainees(self):
     #     self.assertEqual(generate_trainees(), range(20, 31))
