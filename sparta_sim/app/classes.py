0588d0b99cedacd25884b15154e3e5f2168bd931
Kieran <kieran.badesha96@gmail.com>
2021-05-12 12:08:40 +0100

    refactor

--- sparta_sim/app/classes.py
+++ sparta_sim/app/classes.py
@@ -1,3 +1,4 @@
+from numpy.random import binomial
 # Sim Class
 
 class Simulation:
@@ -30,33 +31,36 @@ class TechCentre(Academy):
 
 
 # Trainee Classes
+def trainee_generator(min_trainees, max_trainees):
 
-class Trainee:
-    def __init__(self, min_trainees=20, max_trainees=30):
-        self.min_trainees = min_trainees
-        self.max_trainees = max_trainees
+    course_probabilities = {
+                            'Java': 0.2,
+                            'Data': 0.25,
+                            'CSharp': 1/3,
+                            'DevOps': 0.5,
+                            'Business': 1
+                            }
+
+    trainee_range = max_trainees - min_trainees
+    trainees_per_month = int(binomial(n=trainee_range, p=0.5, size=1))
+    num_of_trainees = min_trainees + trainees_per_month
+
+    if num_of_trainees > max_trainees:
+        num_of_trainees = max_trainees
+
+    trainees = {}
+
+    for course, prob in course_probabilities.items():
+        x = binomial(num_of_trainees, prob)
+
+        num_of_trainees -= x
+
+        trainees[course] = {'Count': x,
+                            'Months Trained': 0}
+
+    return trainees
 
 
-class Java(Trainee):
-    def __init__(self):
-        super().__init__()
 
 
-class Data(Trainee):
-    def __init__(self):
-        super().__init__()
-
-
-class CSharp(Trainee):
-    def __init__(self):
-        super().__init__()
-
-
-class DevOps(Trainee):
-    def __init__(self):
-        super().__init__()
-
-
-class Business(Trainee):
-    def __init__(self):
-        super().__init__()
+print(trainee_generator())
