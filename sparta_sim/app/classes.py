1825610b8f42e7d7a5b30302cd72ea90ca2a9a6e
jagman014 <72930861+jagman014@users.noreply.github.com>
2021-05-12 14:33:00 +0100

    classes and generator update

--- sparta_sim/app/classes.py
+++ sparta_sim/app/classes.py
@@ -9,28 +9,109 @@ class Simulation:
 
 class Academy:
     def __init__(self):
-        pass
+        self.__trainees = {}
+        self.__finished_trainees = {
+                                    'Java': 0,
+                                    'Data': 0,
+                                    'CSharp': 0,
+                                    'DevOps': 0,
+                                    'Business': 0
+                                    }
+        self.__total_trainees = 0
+        self.__capacity = 100
+
+    def update_trainees(self, trainee_dict: dict, month: int):
+        self.queued_trainees = None
+        trainee_dict_total = sum(count['Count'] 
+                                for count in trainee_dict.values()
+                                )
+        spaces = self.__capacity - self.__total_trainees
+
+        if trainee_dict_total > spaces:
+            self.__trainees[f'group_{month}'] = {}
+
+            used_group = []
+
+            for trainee in trainee_dict:
+
+                if (spaces - trainee_dict[trainee]['Count']) >= 0:
+                    self.__trainees[f'group_{month}'][trainee] = (
+                                                        trainee_dict[trainee])
+
+                    spaces -= trainee_dict[trainee]['Count']
+                    used_group.append(trainee)
+            
+            for group in used_group:
+                trainee_dict.pop(group)
+            
+            self.queued_trainees = trainee_dict
+
+        else:
+            self.__trainees[f'group_{month}'] = trainee_dict
+    
+    def increment_trainees(self):
+        for group in self.__trainees.keys():
+            
+            for trainees in self.__trainees[group].keys():
+
+                if self.__trainees[group][trainees]['Months Trained'] == 11:
+                    self.__finished_trainees[trainees] += (
+                                    self.__trainees[group][trainees]['Count'])
+
+                    self.__trainees[group][trainees]['Months Trained'] += 1
+                
+                elif self.__trainees[group][trainees]['Months Trained'] < 11:
+                    self.__trainees[group][trainees]['Months Trained'] += 1
+                
+        self.__update_trainee_count()
+        
+            
+    def get_finished_trainees(self):
+        return sum(self.__finished_trainees.values())
+    
+    def get_trainees(self):
+        return self.__trainees
+    
+    def __update_trainee_count(self):
+        self.__total_trainees = 0
+        for group in self.__trainees.keys():
+
+            for trainees in self.__trainees[group].keys():
+
+                if self.__trainees[group][trainees]['Months Trained'] != 12: 
+                    self.__total_trainees += (
+                                    self.__trainees[group][trainees]['Count']) 
+
+    def get_trainee_count(self):
+        self.__update_trainee_count()
+        return self.__total_trainees
+    
+    def get_queue(self):
+        return self.queued_trainees
 
 
 class TrainingHub(Academy):
 
     def __init__(self):
         super().__init__()
-
+        
 
 class Bootcamp(Academy):
 
     def __init__(self):
         super().__init__()
+        self.__capacity = 500
 
 
 class TechCentre(Academy):
 
-    def __init__(self):
+    def __init__(self, trainee_type):
         super().__init__()
+        self.__capacity = 200
+        self.__trainee_type = trainee_type
 
 
-# Trainee Classes
+# Trainee Generator
 def trainee_generator(min_trainees, max_trainees):
 
     course_probabilities = {
@@ -53,14 +134,12 @@ def trainee_generator(min_trainees, max_trainees):
     for course, prob in course_probabilities.items():
         x = binomial(num_of_trainees, prob)
 
+        while x == 0:
+            x = binomial(num_of_trainees, prob)
+
         num_of_trainees -= x
 
         trainees[course] = {'Count': x,
                             'Months Trained': 0}
 
     return trainees
-
-
-
-
-print(trainee_generator())
